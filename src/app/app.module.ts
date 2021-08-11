e5775c53b5c9c692918125296c6a2e0406b53d78
reshmapatel25 <reshma2505@gmail.com>
2021-08-11 15:38:07 +0530

    updated dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
 import { FormsModule } from '@angular/forms';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
-import { HttpClientModule }    from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RecipesComponent,
-    RecipeDetailComponent,
-    MessagesComponent,
-    DashboardComponent,
-    RecipeSearchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-        InMemoryDataService, { dataEncapsulation: false }
-      )
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
